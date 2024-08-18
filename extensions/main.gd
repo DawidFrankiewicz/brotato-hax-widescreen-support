945591a6fecb9d93aaf2cdf970157306f030512b
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2024-08-18 14:51:27 +0200

    feat: :sparkles: Fix background size in arena

--- extensions/main.gd
+++ extensions/main.gd
@@ -1,7 +1,16 @@
 extends "res://main.gd"
 
-# Changed calculations to fix arena centering
+func _ready():
+	var window_size = get_viewport().get_visible_rect().size
+
+	# Set backgrount width to match window size
+	_background.rect_size.y = window_size.x
+	_background.rect_position.x = window_size.x
+
+# Override calculations to fix arena centering
 func init_camera() -> void:
+	var window_size = get_viewport().get_visible_rect().size
+
 	var max_pos = ZoneService.current_zone_max_position
 	var min_pos = ZoneService.current_zone_min_position
 
@@ -11,13 +20,13 @@ func init_camera() -> void:
 	_camera.center_horizontal_pos = zone_w / 2
 	_camera.center_vertical_pos = zone_h / 2
 	
-	if zone_w + EDGE_SIZE * 2 <= get_viewport().get_visible_rect().size.x:
+	if zone_w + EDGE_SIZE * 2 <= window_size.x:
 		_camera.center_horizontal = true
 	else:
 		_camera.limit_right = max_pos.x + EDGE_SIZE
 		_camera.limit_left = min_pos.x - EDGE_SIZE
 
-	if zone_h + EDGE_SIZE * 2 <= get_viewport().get_visible_rect().size.y:
+	if zone_h + EDGE_SIZE * 2 <= window_size.y:
 		_camera.center_vertical = true
 	else:
 		_camera.limit_bottom = max_pos.y + EDGE_SIZE
