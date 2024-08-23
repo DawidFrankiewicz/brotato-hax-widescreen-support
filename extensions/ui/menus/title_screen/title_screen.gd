c824e5567c188796a9fd07333584553ea428665a
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2024-08-23 21:56:23 +0200

    feat: :sparkles: Adjust main menu art for widescreen

--- extensions/ui/menus/title_screen/title_screen.gd
+++ extensions/ui/menus/title_screen/title_screen.gd
@@ -1,5 +1,9 @@
 extends "res://ui/menus/title_screen/title_screen.gd"
 
+var background_animation_time = 3
+var background_size_multiplier = 1.08
+var animated_background_size_multiplier = 1.16
+
 func _ready():
 	resize_elements()
 	var _size_changed_value = get_tree().get_root().connect("size_changed", self, "resize_elements")
@@ -13,8 +17,43 @@ func resize_elements():
 	margin_left = (window_size.x - 1920) / 2
 	margin_right = -((window_size.x - 1920) / 2)
 
-	# Center background
-	$Background.rect_position = Vector2(-($Background.rect_size.x - 1920) / 2, 0)
-
-	# Center UI
+	# Center UI container
 	$MarginContainer.rect_size = Vector2(1920, 1080)
+
+	# Post processing
+	$BackgroundPostProcessing.rect_size = window_size
+	$BackgroundPostProcessing.stretch_mode = 1
+	$BackgroundPostProcessing.rect_position.x = -((window_size.x - 1920) / 2)
+
+	# Background
+	$Background.rect_size.x = window_size.x * background_size_multiplier
+	$Background.stretch_mode = 1
+	$Background.rect_position = Vector2(-((window_size.x * background_size_multiplier - 1920) / 2), 0)
+
+	# Background Mist Back
+	$BackgroundMistBack/AnimationPlayer.stop()
+	$BackgroundMistBack.rect_size = Vector2(window_size.x * animated_background_size_multiplier, window_size.y)
+	$BackgroundMistBack.stretch_mode = 1
+	$BackgroundMistBack.rect_position.x = -((window_size.x * animated_background_size_multiplier - 1920) / 2)
+
+	## Recreate animation with tween
+	var tween_back = get_tree().create_tween().set_loops().set_trans(Tween.TRANS_QUAD)
+	tween_back.tween_property($BackgroundMistBack, "rect_position", Vector2(-((window_size.x - 1920) / 2), 0), background_animation_time)
+	tween_back.tween_property($BackgroundMistBack, "rect_position", Vector2(-((window_size.x * animated_background_size_multiplier - 1920) / 2), 0), background_animation_time)
+
+
+	# Background Mist Mid
+	$BackgroundMistMid.rect_size = window_size
+	$BackgroundMistMid.stretch_mode = 1
+	$BackgroundMistMid.rect_position.x = -((window_size.x - 1920) / 2)
+
+	# Background Mist Front
+	$BackgroundMistFront/AnimationPlayer.stop()
+	$BackgroundMistFront.rect_size = Vector2(window_size.x * animated_background_size_multiplier, window_size.y)
+	$BackgroundMistFront.stretch_mode = 1
+	$BackgroundMistFront.rect_position.x = -((window_size.x - 1920) / 2)
+
+	## Recreate animation with tween
+	var tween_front = get_tree().create_tween().set_loops().set_trans(Tween.TRANS_QUAD)
+	tween_front.tween_property($BackgroundMistFront, "rect_position", Vector2(-((window_size.x * animated_background_size_multiplier - 1920) / 2), 0), background_animation_time)
+	tween_front.tween_property($BackgroundMistFront, "rect_position", Vector2(-((window_size.x - 1920) / 2), 0), background_animation_time)
