02153bec81b185d5cc580b7f4841e053d59fcb53
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2024-08-23 21:58:31 +0200

    fix: :fire: Remove unnecessary scripts

--- mod_main.gd
+++ mod_main.gd
@@ -5,8 +5,6 @@ func _init() -> void:
 	ModLoaderMod.install_script_extension(dir + "/extensions/main.gd")
 	ModLoaderMod.install_script_extension(dir + "/extensions/shop.gd")
 	ModLoaderMod.install_script_extension(dir + "/extensions/ui/menus/run/character_selection.gd")
-	ModLoaderMod.install_script_extension(dir + "/extensions/ui/menus/run/weapons_selection.gd")
-	ModLoaderMod.install_script_extension(dir + "/extensions/ui/menus/run/difficulty_selection/difficulty_selection.gd")
 	ModLoaderMod.install_script_extension(dir + "/extensions/ui/menus/title_screen/title_screen.gd")
 
 func _ready() -> void:
